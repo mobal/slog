e89c84e2e28e67654b07e70ecd29f4b308f63b6b
mobal <the.mobal@gmail.com>
2020-08-02 00:31:15 +0200

    Added published_at timestamp to posts table and indices

--- src/main/resources/db/migration/V1__init_default_schema.sql
+++ src/main/resources/db/migration/V1__init_default_schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS `posts`(
     `title` VARCHAR(255) NOT NULL,
     `created_at` TIMESTAMP,
     `deleted_at` TIMESTAMP,
+    `published_at` TIMESTAMP,
     PRIMARY KEY(`id`)
 );
 
@@ -34,3 +35,8 @@ CREATE TABLE IF NOT EXISTS `posts_tags`(
 );
 
 ALTER TABLE `metas` ADD CONSTRAINT `fk_post_id` FOREIGN KEY(`post_id`) REFERENCES `posts`(`id`);
+
+CREATE INDEX `idx_author` ON `posts`(`author`);
+
+CREATE INDEX `idx_title` ON `posts`(`title`);
+CREATE INDEX `idx_name` ON `tags`(`name`);
