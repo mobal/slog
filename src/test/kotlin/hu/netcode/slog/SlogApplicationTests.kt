adb69aaa603d5199dbcd3a2730e1f81f0876440c
mobal <the.mobal@gmail.com>
2020-08-15 15:20:42 +0200

    Removed unnecessary annotation

--- src/test/kotlin/hu/netcode/slog/SlogApplicationTests.kt
+++ src/test/kotlin/hu/netcode/slog/SlogApplicationTests.kt
@@ -1,10 +1,8 @@
 package hu.netcode.slog
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.test.context.SpringBootTest
 
-@ConfigurationPropertiesScan
 @SpringBootTest
 class SlogApplicationTests {
     @Test
