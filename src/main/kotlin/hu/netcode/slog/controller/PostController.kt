2bd9ff34057d60fe0ad4e509258172e5c86dc6cb
mobal <the.mobal@gmail.com>
2020-08-15 15:21:01 +0200

    Added validation annotation

--- src/main/kotlin/hu/netcode/slog/controller/PostController.kt
+++ src/main/kotlin/hu/netcode/slog/controller/PostController.kt
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController
     produces = [MediaType.APPLICATION_JSON_VALUE],
     value = ["/api/posts"]
 )
+@Validated
 class PostController(
     private val pagingProperties: PagingProperties,
     private val postService: PostService
