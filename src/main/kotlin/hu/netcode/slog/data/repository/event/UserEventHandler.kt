be52ccde37f770caf6252ad4f89f90df647f35f6
mobal <the.mobal@gmail.com>
2020-08-15 11:56:52 +0200

    Fixed linting error

--- src/main/kotlin/hu/netcode/slog/data/repository/event/UserEventHandler.kt
+++ src/main/kotlin/hu/netcode/slog/data/repository/event/UserEventHandler.kt
@@ -3,7 +3,6 @@ package hu.netcode.slog.data.repository.event
 import hu.netcode.slog.data.entity.User
 import org.slf4j.LoggerFactory
 import org.springframework.data.rest.core.annotation.HandleAfterCreate
-import org.springframework.data.rest.core.annotation.HandleBeforeCreate
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler
 
 @RepositoryEventHandler(value = [User::class])
