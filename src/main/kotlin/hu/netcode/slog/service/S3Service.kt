d823ee5be87bd000994b4f1757e73d1f111d361d
mobal <the.mobal@gmail.com>
2020-08-21 00:37:52 +0200

    Added function implementations

--- src/main/kotlin/hu/netcode/slog/service/S3Service.kt
+++ src/main/kotlin/hu/netcode/slog/service/S3Service.kt
@@ -2,8 +2,16 @@ package hu.netcode.slog.service
 
 import com.amazonaws.AmazonClientException
 import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.Bucket
+import com.amazonaws.services.s3.model.CopyObjectResult
+import com.amazonaws.services.s3.model.DeleteObjectsRequest
+import com.amazonaws.services.s3.model.DeleteObjectsResult
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectResult
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.io.IOException
+import java.io.InputStream
 
 @Service
 class S3Service(
@@ -11,28 +19,71 @@ class S3Service(
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    fun copyObject() {
-        //
+    fun copyObject(
+        sourceBucketName: String,
+        sourceKey: String,
+        destinationBucketName: String,
+        destinationKey: String
+    ): CopyObjectResult {
+        try {
+            return s3Client.copyObject(sourceBucketName, sourceKey, destinationBucketName, destinationKey)
+        } catch (ex: AmazonClientException) {
+            logger.error("Failed to copy object {} as {} from bucket {} to {}", sourceKey, destinationKey,
+                sourceBucketName, destinationBucketName)
+            throw ex
+        }
     }
 
-    fun createBucket() {
-        //
+    fun createBucket(bucketName: String): Bucket {
+        try {
+            return s3Client.createBucket(bucketName)
+        } catch (ex: AmazonClientException) {
+            logger.error("Failed to create bucket because of an exception: {}", ex)
+            throw ex
+        }
     }
 
-    fun deleteBucket() {
-        //
+    fun deleteBucket(bucketName: String) {
+        try {
+            s3Client.deleteBucket(bucketName)
+        } catch (ex: AmazonClientException) {
+            logger.error("Failed to delete bucket {} because of an exception: {}", bucketName, ex)
+            throw ex
+        }
     }
 
-    fun deleteObject() {
-        //
+    fun deleteObject(bucketName: String, key: String) {
+        try {
+            s3Client.deleteObject(bucketName, key)
+        } catch (ex: AmazonClientException) {
+            logger.error("Failed to delete object {} because of an exception: {}", key, ex)
+            throw ex
+        }
     }
 
-    fun deleteObjects() {
-        //
+    fun deleteObjects(bucketName: String, objectKeyList: List<String>): DeleteObjectsResult {
+        try {
+            val req = DeleteObjectsRequest(bucketName)
+            req.keys = objectKeyList.map { DeleteObjectsRequest.KeyVersion(it) }.toList()
+            return s3Client.deleteObjects(req)
+        } catch (ex: AmazonClientException) {
+            logger.error("Failed to delete objects from bucket {} because of an exception: {}", bucketName, ex)
+            throw ex
+        }
     }
 
-    fun getObject() {
-        //
+    fun getObject(bucketName: String, key: String): ByteArray {
+        try {
+            return s3Client.getObject(bucketName, key)
+                .objectContent
+                .readAllBytes()
+        } catch (ex: AmazonClientException) {
+            logger.error("Failed to get object {} from bucket {} because of an exception: {}", key, bucketName, ex)
+            throw ex
+        } catch (ex: IOException) {
+            logger.error("Failed to convert object {} to byte array because of an exception: {}", key, ex)
+            throw ex
+        }
     }
 
     fun listBuckets(): List<String> {
@@ -46,19 +97,26 @@ class S3Service(
         }
     }
 
-    fun listObjects(bucket: String): List<String> {
+    fun listObjects(bucketName: String): List<String> {
         try {
-            return s3Client.listObjects(bucket)
+            return s3Client.listObjects(bucketName)
                 .objectSummaries
                 .map { it.key }
                 .toList()
         } catch (ex: AmazonClientException) {
-            logger.error("Failed to list object from bucket {} because of an exception: {}", bucket, ex)
+            logger.error("Failed to list object from bucket {} because of an exception: {}", bucketName, ex)
             throw ex
         }
     }
 
-    fun putObject() {
-        //
+    fun putObject(bucketName: String, key: String, data: InputStream, mime: String): PutObjectResult {
+        try {
+            val metaData = ObjectMetadata()
+            metaData.contentType = "meta"
+            return s3Client.putObject(bucketName, key, data, metaData)
+        } catch (ex: AmazonClientException) {
+            logger.error("Failed to put object because of an exception: {}", ex)
+            throw ex
+        }
     }
 }
