ddbf4889f1afdecbc1a66b3a3d430e0c0a9316d7
mobal <the.mobal@gmail.com>
2020-08-21 01:27:32 +0200

    Added modelmapper and updated s3 configuration

--- src/main/kotlin/hu/netcode/slog/SlogApplication.kt
+++ src/main/kotlin/hu/netcode/slog/SlogApplication.kt
@@ -1,12 +1,15 @@
 package hu.netcode.slog
 
+import com.amazonaws.ClientConfiguration
 import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.github.slugify.Slugify
-import hu.netcode.slog.properties.AWSProperties
+import hu.netcode.slog.properties.S3Properties
+import org.modelmapper.ModelMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
@@ -18,7 +21,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 @EnableJpaRepositories
 @SpringBootApplication
 class SlogApplication(
-    private val awsProperties: AWSProperties
+    private val s3Properties: S3Properties
 ) {
     @Bean
     fun bcryptPasswordEncoder(): BCryptPasswordEncoder {
@@ -26,12 +29,24 @@ class SlogApplication(
     }
 
     @Bean
+    fun modelMapper(): ModelMapper {
+        return ModelMapper()
+    }
+
+    @Bean
     fun s3Client(): AmazonS3 {
         return AmazonS3ClientBuilder.standard()
+            .withClientConfiguration(ClientConfiguration().apply {
+                signerOverride = "AWSS3V4SignerType"
+            })
             .withCredentials(AWSStaticCredentialsProvider(
-                BasicAWSCredentials(awsProperties.accessKey, awsProperties.secretKey))
+                BasicAWSCredentials(s3Properties.accessKey, s3Properties.secretKey))
             )
-            .withRegion(Regions.fromName(awsProperties.region))
+            .withEndpointConfiguration(
+                AwsClientBuilder.EndpointConfiguration("${s3Properties.host}:${s3Properties.port}",
+                    Regions.fromName(s3Properties.region).name)
+            )
+            .withPathStyleAccessEnabled(true)
             .build()
     }
 
