b1b0f7489572a3a94e1a5304e2f6dd2504a47f07
mobal <the.mobal@gmail.com>
2020-08-25 23:30:36 +0200

    Added AmazonClientException handling

--- src/main/kotlin/hu/netcode/slog/ExceptionHandler.kt
+++ src/main/kotlin/hu/netcode/slog/ExceptionHandler.kt
@@ -9,6 +9,7 @@ import javax.validation.ConstraintViolationException
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -23,28 +24,37 @@ class ExceptionHandler(
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
+    @ExceptionHandler(value = [AmazonClientException::class])
+    fun handleAmazonClientException(req: HttpServletRequest, ex: AmazonClientException):
+            ResponseEntity<Map<String, Any>> {
+        logger.error("{} {} {}", ex::class, req, ex)
+        val httpStatus = HttpStatus.valueOf("(Status Code: \\d{3})".toRegex().find(ex.message!!)?.value
+            ?.takeLast(3)!!.toInt())
+        return ResponseEntity(exceptionService.createResponseMap(ex, httpStatus), httpStatus)
+    }
+
     @ExceptionHandler(value = [ConstraintViolationException::class])
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    fun handleConstraintViolationException(req: HttpServletRequest, ex: Exception): Map<String, Any> {
+    fun handleConstraintViolationException(req: HttpServletRequest, ex: ConstraintViolationException):
+            Map<String, Any> {
         logger.error("ConstraintViolationException {} {}", req, ex)
         return exceptionService.createResponseMap(ex, HttpStatus.BAD_REQUEST)
     }
 
     @ExceptionHandler(value = [EntityNotFoundException::class])
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    fun handleEntityNotFoundException(req: HttpServletRequest, ex: java.lang.Exception): Map<String, Any> {
+    fun handleEntityNotFoundException(req: HttpServletRequest, ex: EntityNotFoundException): Map<String, Any> {
         logger.error("EntityNotFoundException {} {}", req, ex)
         return exceptionService.createResponseMap(ex, HttpStatus.NOT_FOUND)
     }
 
     @ExceptionHandler(value = [
-        AmazonClientException::class,
         Exception::class,
         NullPointerException::class
     ])
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     fun handleException(req: HttpServletRequest, ex: Exception): Map<String, Any> {
-        logger.error("Exception {} {}", req, ex)
+        logger.error("{} {} {}", ex::class, req, ex)
         return exceptionService.createResponseMap(ex, HttpStatus.INTERNAL_SERVER_ERROR)
     }
 }
