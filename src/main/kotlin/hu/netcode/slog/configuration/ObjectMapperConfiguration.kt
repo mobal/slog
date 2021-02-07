da83c7004720877a8dba8ea13145eca3ee0c24ae
mobal <the.mobal@gmail.com>
2021-02-07 20:36:07 +0100

    Fixed linting errors

--- src/main/kotlin/hu/netcode/slog/configuration/ObjectMapperConfiguration.kt
+++ src/main/kotlin/hu/netcode/slog/configuration/ObjectMapperConfiguration.kt
@@ -1,7 +1,6 @@
 package hu.netcode.slog.configuration
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
